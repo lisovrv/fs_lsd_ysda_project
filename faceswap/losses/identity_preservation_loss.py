65ba4127e0292faedeeebcb232a419ab6e5aa983
MakArtKar <makoyan2001@gmail.com>
2023-03-22 00:40:40 +0200

    Fixed bugs

--- faceswap/losses/identity_preservation_loss.py
+++ faceswap/losses/identity_preservation_loss.py
@@ -2,33 +2,11 @@ import torch
 from torch import nn as nn, Tensor
 
 
-from AdaptiveWingLoss.core import models
-from faceswap.utils import detect_landmarks
-
-
 class IdentityPreservationLoss(nn.Module):
-    def __init__(self, path_to_model: str):
+    def __init__(self):
         super().__init__()
         self.loss = nn.CosineEmbeddingLoss()
-        self.model_ft = models.FAN(4, "False", "False", 98)
-        self.setup_model(path_to_model)
 
-    def setup_model(self, path_to_model: str):
-        checkpoint = torch.load(path_to_model, map_location='cpu')
-        if 'state_dict' not in checkpoint:
-            self.model_ft.load_state_dict(checkpoint)
-        else:
-            pretrained_weights = checkpoint['state_dict']
-            model_weights = self.model_ft.state_dict()
-            pretrained_weights = {k: v for k, v in pretrained_weights.items() \
-                                  if k in model_weights}
-            model_weights.update(pretrained_weights)
-            self.model_ft.load_state_dict(model_weights)
-        self.model_ft.eval()
-
-    def forward(self, source: Tensor, final: Tensor, **kwargs):
-        batch_size = source.size(0)
-        source_emb = detect_landmarks(source, self.model_ft).view(batch_size, -1)  # Add denormalize
-        final_emb = detect_landmarks(final, self.model_ft).view(batch_size, -1)  # Add denormalize
-        labels = torch.full((batch_size,), 1).to(final_emb.device).long()
+    def forward(self, final_emb: Tensor, source_emb: Tensor, **kwargs):
+        labels = torch.full((final_emb.size(0),), 1).to(final_emb.device).long()
         return self.loss(final_emb, source_emb, labels)
--- faceswap/losses/landmark_alignment_loss.py
+++ faceswap/losses/landmark_alignment_loss.py
@@ -1,10 +1,36 @@
 import torch
 from torch import nn as nn, Tensor
 
+from AdaptiveWingLoss.core import models
+from faceswap.utils import detect_landmarks
+
 
 class LandmarkAlignmentLoss(nn.Module):
-    def forward(self, side_lmk: Tensor, final_lmk: Tensor, target_lmk: Tensor, **kwargs):
-        batch_size = side_lmk.size(0)
-        return (torch.norm((side_lmk - target_lmk).view(batch_size, -1), dim=1) +
-                torch.norm((final_lmk - target_lmk).view(batch_size, -1), dim=1)
-                ).mean()
+    def __init__(self, path_to_model: str):
+        super().__init__()
+        self.model_ft = models.FAN(4, "False", "False", 98)
+        self.setup_model(path_to_model)
+
+    def setup_model(self, path_to_model: str):
+        checkpoint = torch.load(path_to_model, map_location='cpu')
+        if 'state_dict' not in checkpoint:
+            self.model_ft.load_state_dict(checkpoint)
+        else:
+            pretrained_weights = checkpoint['state_dict']
+            model_weights = self.model_ft.state_dict()
+            pretrained_weights = {k: v for k, v in pretrained_weights.items() \
+                                  if k in model_weights}
+            model_weights.update(pretrained_weights)
+            self.model_ft.load_state_dict(model_weights)
+        self.model_ft.eval()
+
+    def forward(self, side: Tensor, final: Tensor, target: Tensor, **kwargs):
+        # TODO: Add denormalize
+        batch_size = side.size(0)
+        side_lmk = detect_landmarks(side, self.model_ft).view(batch_size, -1)
+        final_lmk = detect_landmarks(final, self.model_ft).view(batch_size, -1)
+        target_lmk = detect_landmarks(target, self.model_ft).view(batch_size, -1)
+        return (
+            torch.norm((side_lmk - target_lmk).view(batch_size, -1), dim=1) +
+            torch.norm((final_lmk - target_lmk).view(batch_size, -1), dim=1)
+        ).mean()
