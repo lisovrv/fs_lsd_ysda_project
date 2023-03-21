77d09e70de52e9447496adae83c25738178f5397
MakArtKar <makoyan2001@gmail.com>
2023-03-21 21:36:17 +0200

    refactored reconstruction

--- faceswap/losses/reconstruction_loss.py
+++ faceswap/losses/reconstruction_loss.py
@@ -1,3 +1,6 @@
+from typing import List
+
+import lpips
 import torch
 from torch import nn as nn, Tensor
 
@@ -6,6 +9,7 @@ class ReconstructionLoss(nn.Module):
     def __init__(self, alpha: float):
         super().__init__()
         self.alpha = torch.tensor([alpha])
+        self.loss_fn_vgg = lpips.LPIPS(net='vgg')
 
     def forward(
         self,
@@ -13,18 +17,13 @@ class ReconstructionLoss(nn.Module):
         target: Tensor,
         side: Tensor,
         final: Tensor,
-        target_features: Tensor,
-        side_features: Tensor,
-        final_features: Tensor,
         **kwargs,
     ):
         if torch.allclose(source, target):
             batch_size = source.size(0)
-            return (
-                    torch.norm((final - target).view(batch_size, -1), dim=1) +
-                    self.alpha * torch.norm((final_features - target_features).view(batch_size, -1), dim=1) +
-                    torch.norm((side - target).view(batch_size, -1), dim=1) +
-                    self.alpha * torch.norm((side_features - target_features).view(batch_size, -1), dim=1)
-            ).mean()
+            loss = torch.norm((final - target).view(batch_size, -1), dim=1) + \
+                torch.norm((side - target).view(batch_size, -1), dim=1)
+            lpips_loss = self.loss_fn_vgg(final, target) + self.loss_fn_vgg(side, target)
+            return loss + self.alpha * lpips_loss
         else:
             return 0.
